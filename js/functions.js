726e7793080e10c2d4143384a2355fe770d5d2ae
sseabrease <sseabrease@fearsol.com>
2019-01-11 14:37:22 -0500

    Fixed jumping to top of page

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,15 @@
 
 $(document).ready(function() {
-  $("#hide-ideas").click(function(){
+  $("#hide-ideas").click(function(e){
+      e.preventDefault();
       $(".ideas__form").hide( "blind", {direction: "vertical"}, 1000 );
       $('html,body').animate({
         scrollTop: $("#ideas").offset().top},
         'slow');
    });
 
-   $("#show-ideas").click(function(){
+   $("#show-ideas").click(function(e){
+      e.preventDefault();
       $(".ideas__form").show( "blind", {direction: "vertical"}, 1000 );
       $('html,body').animate({
         scrollTop: $("#ideas").offset().top},
